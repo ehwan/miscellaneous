adecf48c97dad0f128835eeb2baa8c487dff69ac
ehwank <ehwank98@gmail.com>
2016-02-19 14:00:37 +0900

    member types

--- EHUtil/Memory.h
+++ EHUtil/Memory.h
@@ -6,17 +6,27 @@
 namespace EH
 {
     template < typename T >
-    struct Ptr
+    class Ptr
     {
+    public:
         using value_type = T;
+        using const_type = const value_type;
 
+        using reference_type = T&;
+        using const_reference_type = const_type&;
+
+        using pointer_type = T*;
+        using const_pointer_type = const_type*;
+
+    protected:
         template < typename T0 >
         using other_type = Ptr< T0 >;
 
         using this_type  = other_type< T >;
 
-        value_type *ptr;
+        pointer_type ptr;
 
+    public:
         Ptr() :
             ptr( 0 )
         {
@@ -25,7 +35,7 @@ namespace EH
         {
             ptr = new value_type[ _count ];
         }
-        Ptr( value_type *rhs )
+        Ptr( pointer_type rhs )
         {
             ptr = rhs;
         }
@@ -39,6 +49,7 @@ namespace EH
             release();
         }
 
+    protected:
         void retain()
         {
         }
@@ -54,15 +65,20 @@ namespace EH
         template < typename T2 >
         void move_from( other_type< T2 >& rhs )
         {
-            ptr = reinterpret_cast< value_type* >( rhs.ptr );
-            rhs.ptr = 0;
+            ptr = reinterpret_cast< pointer_type >( rhs() );
+            rhs() = 0;
         }
         template < typename T2 >
         void copy_from( const other_type< T2 >& rhs )
         {
-            ptr = reinterpret_cast< value_type* >( rhs.ptr );
+            ptr = reinterpret_cast< pointer_type >( rhs() );
         }
 
+    public:
+        void reset()
+        {
+            release();
+        }
         template < typename T2 >
         this_type& operator = ( other_type< T2 >&& rhs )
         {
@@ -71,7 +87,7 @@ namespace EH
 
             return *this;
         }
-        this_type& operator = ( value_type *rhs )
+        this_type& operator = ( pointer_type rhs )
         {
             release();
             ptr = rhs;
@@ -79,46 +95,46 @@ namespace EH
             return *this;
         }
 
-        value_type* operator -> ()
+        pointer_type operator -> ()
         {
             return ptr;
         }
-        const value_type* operator -> () const
-        {
-            return ptr;
-        }
-
-        value_type* data()
-        {
-            return ptr;
-        }
-        const value_type* data() const
+        pointer_type operator -> () const
         {
             return ptr;
         }
 
-        value_type* operator () ()
+        pointer_type data()
         {
             return ptr;
         }
-        const value_type* operator () () const
+        pointer_type data() const
         {
             return ptr;
         }
-        value_type* operator () ( std::size_t i )
+
+        pointer_type& operator () ()
+        {
+            return ptr;
+        }
+        const pointer_type& operator () () const
+        {
+            return ptr;
+        }
+        pointer_type& operator () ( std::size_t i )
         {
             return ptr + i;
         }
-        const value_type* operator () ( std::size_t i ) const
+        const pointer_type& operator () ( std::size_t i ) const
         {
             return ptr + i;
         }
 
-        value_type& operator [] ( std::size_t i )
+        reference_type operator [] ( std::size_t i )
         {
             return ptr[ i ];
         }
-        const value_type& operator [] ( std::size_t i ) const
+        const_reference_type operator [] ( std::size_t i ) const
         {
             return ptr[ i ];
         }
