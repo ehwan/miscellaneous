b3447c71fb5cb89ad1098f7da5525834d81144a9
ehwank <ehwank98@gmail.com>
2016-02-22 20:17:38 +0900

    return type to auto

--- EHUtil/EHFile.h
+++ EHUtil/EHFile.h
@@ -11,8 +11,10 @@ namespace EH
     namespace File
     {
         template < typename RET = char , typename SizeType >
-        Ptr< RET , std::default_delete< RET[] > > LoadFile( const char *name , std::ios_base::openmode mode , SizeType size )
+        auto LoadFile( const char *name , std::ios_base::openmode mode , SizeType&& size )
         {
+            using remove_ref = typename std::remove_reference< SizeType >::type;
+            using remove_con = typename std::remove_const< remove_ref >::type;
             std::ifstream fbuf( name , mode );
 
             fbuf.seekg( 0 , std::ios_base::end );
@@ -20,26 +22,26 @@ namespace EH
             LOG( "File Load Text : \n" , name , "\n" , sz , " bytes\n" );
             fbuf.seekg( 0 , std::ios_base::beg );
 
-            size = static_cast< SizeType >( sz );
+            size = static_cast< remove_con >( sz );
 
             char *ptr = new char[ sz + 1 ];
             ptr[ sz ] = 0;
             fbuf.read( ptr , sz );
             fbuf.close();
 
-            return Ptr< RET >( reinterpret_cast< RET* >( ptr ) );
+            return Ptr< RET , std::default_delete< RET[] > >( reinterpret_cast< RET* >( ptr ) );
         }
 
 
         template < typename RET = char , typename SizeType >
-        Ptr< RET , std::default_delete< RET[] > > LoadText( const char *name , SizeType size )
+        auto LoadText( const char *name , SizeType&& size )
         {
-            return LoadFile< RET >( name , std::ios_base::in , size );
+            return LoadFile< RET >( name , std::ios_base::in , std::forward< SizeType >( size ) );
         }
         template < typename RET = char , typename SizeType >
-        Ptr< RET , std::default_delete< RET[] > > LoadBinary( const char *name , SizeType size )
+        auto LoadBinary( const char *name , SizeType&& size )
         {
-            return LoadFile< RET >( name , std::ios_base::in | std::ios_base::binary , size );
+            return LoadFile< RET >( name , std::ios_base::in | std::ios_base::binary , std::forward< SizeType >( size ) );
         }
     };  // namespace File
 };  // namespace EH
